5fa483a3489622a0c1ed414c5485a564f7a39548
link00000000 <crandall.logan@gmail.com>
2021-05-10 15:27:02 -0400

    Setup deployment

--- setup.py
+++ setup.py
@@ -1,28 +1,63 @@
+import pathlib
 from setuptools import setup, find_packages
- 
-classifiers = [
-  'Development Status :: 4 - Beta',
-  'Intended Audience :: Developers',
-  'Operating System :: Microsoft :: Windows',
-  'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3'
-]
+
+root = pathlib.Path(__file__).parent
+
+README = (root/'README.md').read_text()
 
 setup(
-  name='global_hotkeys',
-  version='0.0.4',
-  description='',
-  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
-  long_description_content_type='text/markdown',
-  url='https://github.com/btsdev',
-  author='btsdev',
-  author_email='btsdevman@gmail.com',
-  license='MIT',
-  classifiers=classifiers,
-  keywords='hotkeys,shortcuts',
-  packages=find_packages(),
-  install_requires=[], # "some_lib ~= 1.0"
-  extras_require = {
-    "dev": [] # "pytest>=3.7"
-  }
+    name="aioglobal-hotkeys",
+    version="0.1.0",
+    description="A global hotkey binding manager for Windows using Python coroutines",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/link00000000/aioglobal-hotkeys",
+    author="Logan Crandall",
+    author_email="logan@accidentallycoded.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="hotkeys,shortcuts,asyncio,aio,async",
+    packages=find_packages(exclude=("examples")),
+    install_requires=["pywin32==300"],
+    extras_require={
+        "dev": [
+            "appdirs==1.4.4",
+            "attrs==21.2.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+            "black==19.10b0; python_version >= '3.6'",
+            "cached-property==1.5.2",
+            "cerberus==1.3.4",
+            "certifi==2020.12.5",
+            "chardet==4.0.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+            "click==7.1.2; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+            "colorama==0.4.4; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+            "distlib==0.3.1",
+            "idna==2.10; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "orderedmultidict==1.0.1",
+            "packaging==20.9; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "pathspec==0.8.1",
+            "pep517==0.10.0",
+            "pip-shims==0.5.3; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+            "pipenv-setup==3.1.1",
+            "pipfile==0.0.2",
+            "plette[validation]==0.2.3; python_version >= '2.6' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "pyparsing==2.4.7; python_version >= '2.6' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "python-dateutil==2.8.1; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "regex==2021.4.4",
+            "requests==2.25.1; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+            "requirementslib==1.5.16; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+            "six==1.16.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "toml==0.10.2; python_version >= '2.6' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "tomlkit==0.7.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+            "typed-ast==1.4.3",
+            "urllib3==1.26.4; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4' and python_version < '4'",
+            "vistir==0.5.2; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+            "wheel==0.36.2; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+        ]
+    },
 )
